36f5bebe6972618d3954f5d585a4fb62e8eac639
self-harm <lightgraywrite@gmail.com>
2021-01-20 01:56:19 +0300

    Add files via upload

---
+++ JDBC_practice/exam/src/main/java/io/harmed/DBWorker.java
@@ -0,0 +1,23 @@
+package io.harmed;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBWorker {
+    private static final String URL = "jdbc:mysql://localhost:3306/exam_db?useUnicode=true&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+
+    public DBWorker() {
+        try{
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Произошла ошибка при подключении к БД!");
+        }
+    }
+}
---
+++ JDBC_practice/exam/src/main/java/io/harmed/User.java
@@ -0,0 +1,42 @@
+package io.harmed;
+
+public class User {
+    private String name;
+    private String password;
+    private int id;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(String name, String password, int id) {
+        this.name = name;
+        this.password = password;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
